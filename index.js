924b9cc7064d3779d3dc48458afbd147161bc3cc
Marius <grosskopf81@gmail.com>
2021-12-10 22:31:47 +0100

    remove wrong urls

--- index.js
+++ index.js
@@ -1,63 +1,74 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const url = require('url');
 const fs = require('fs');
-
+let browser = null;
 const websites = [];
 
 
 (async () => {
-    
+    browser = await puppeteer.launch();
     let googleContent = await getWebsiteContent('https://www.google.de/search?q=glynt+revital+regain+shot');
     let anchors = getAnchors(googleContent);
     let structuredData = []
     anchors.forEach(async url => {
         let websiteContent = await getWebsiteContent(url);
         const $ = cheerio.load(websiteContent);
-        
-        $("script").each(function() {
-            console.log($(this))
-            if ($(this).attr("type") == "application/ld+json") {
-                let temp = $(this).html()
-                let tempJson = JSON.parse(temp)
-                if (tempJson["@type"].toLowerCase() == "product") {
-                    structuredData.push(temp)
+        $("script").each(async function () {
+            if (isStructuredData($(this).attr("type"))) {
+                let tempJson = JSON.parse($(this).html());
+                if (isProduct(tempJson["@type"])) {
+                    tempJson = await transformJson(tempJson, url);
+                    structuredData.push(JSON.stringify(tempJson));
                 }
-                
             }
         })
         fs.writeFile("sd.json", structuredData, (err) => {
-            if (err)
-              console.log(err);
-            else {
-              console.log("File written successfully\n");
-              console.log("The written has the following contents:");
-            }
-          });
-       // application/ld+json
+           console.log("err", err);
+        });
     })
    
 
 })();
 
+function transformJson(data, url) {
+    data.correctUrl = url;
+    data.baseUrl = new URL(url).origin
+    return data
+}
+
+function isStructuredData(that) {
+    return that == "application/ld+json"
+}
+
+function isProduct(data){
+   return data.toLowerCase() == "product"
+}
+
 
 async function getWebsiteContent(url) {
-    const browser = await puppeteer.launch();
+    console.log("url", url);
     const page = await browser.newPage();
     await page.goto(url, {
       waitUntil: 'networkidle2',
     });
     const content = await page.content();
-    await browser.close();
+    await page.close()
     return content;
 }
 
 function getAnchors(content) {
     const $ = cheerio.load(content);
     let anchors = [];
-     $('a.plantl').each(function() {
+     $('a').each(function() {
          let ref = $(this).attr("href");
-         if (anchors.indexOf(ref) == -1) {
-          anchors.push(ref)
+         if (!ref) return
+         fs.appendFileSync("urls.json", ref, (err) => {
+            console.log(err);
+         });
+         if (anchors.indexOf(ref) == -1 && ref) {
+            if (ref.substring(0,4).toLowerCase() != "http") return
+            anchors.push(ref)
          }
     })
     return anchors;
