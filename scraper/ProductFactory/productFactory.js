6c23630868a4a1d326a16b5443ec035b7b41970d
Marius <grosskopf81@gmail.com>
2022-03-16 20:18:04 +0100

    add mongo db

--- scraper/ProductFactory/productFactory.js
+++ scraper/ProductFactory/productFactory.js
@@ -1,9 +1,24 @@
 exports.dataToProducts = function(crawledData) {
     let products = [];
-    console.log(crawledData[0])
     crawledData.forEach(rawProduct => {
         rawProduct = JSON.parse(rawProduct)
-
+        /* let tempProduct = {
+                    name: rawProduct.name || "",
+                    brand: rawProduct.brand,
+                    image: rawProduct.image,
+                    description: rawProduct.description,
+                    sku: rawProduct.sku,
+                    gtin13: rawProduct.gtin13,
+                    offers: rawProduct.offers,
+                    aggregateRating: {
+                        bestRating: rawProduct.bestRating,
+                        worstRating: rawProduct.worstRating,
+                        ratingValue: rawProduct.ratingValue,
+                        reviewCount: rawProduct.reviewCount,
+                        ratingCount: rawProduct.ratingCount
+                    },
+                    review: rawProduct.review
+                } */
         products.push(rawProduct)
     })
     return products
