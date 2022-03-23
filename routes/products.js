fda84a751e05586039c1a8845830615e69ec59d2
Marius <grosskopf81@gmail.com>
2022-03-23 21:59:33 +0100

    remove express-mongo

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const scraper = require('../scraper/scraper')
 
 router.post('/', async (req, res, next) => {
     console.log(req.body)
-    if(!req.body.terms) return
+    if(!req.body.terms) return res.status(400).send("no terms provided")
     let terms = req.body.terms.split(",")
     let data = await scraper.getData(terms)
     res.json(data)
